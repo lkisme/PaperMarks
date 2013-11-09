ead08255638467b24e2eeea64ad66a766c2297c6
helloyou2012 <helloyou2012@yeah.net>
2013-11-09 11:18:42 +0800

    Initial Commit

---
+++ PaperMarks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaperMarks
+//
+//  Created by ZhenzhenXu on 11/9/13.
+//  Copyright (c) 2013 ZhenzhenXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaperMarks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PaperMarks
+//
+//  Created by ZhenzhenXu on 11/9/13.
+//  Copyright (c) 2013 ZhenzhenXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
